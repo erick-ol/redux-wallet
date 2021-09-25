8bfc9210b7701958861e81f3d1ab92398f132cdd
Erick Santos <erickosantos.dev@gmail.com>
2021-09-25 18:19:14 -0300

    Req 1 ao 3

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { createSlice } from '@reduxjs/toolkit';
+
+const slice = createSlice({
+  name: 'user',
+  initialState: {
+    email: '',
+  },
+  reducers: {
+    setUserEmail: (state, action) => { state.email = action.payload; },
+  },
+});
+
+export const { setUserEmail } = slice.actions;
+export default slice.reducer;
