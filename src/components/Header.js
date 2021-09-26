80262806342647669f18095fb38be17133d27171
Erick Santos <erickosantos.dev@gmail.com>
2021-09-26 19:21:42 -0300

    Req. 5 - arrumando

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const Header = ({ email, total }) => (
-  <header>
-    <span data-testid="email-field">{`E-mail: ${email}`}</span>
-    <span data-testid="total-field">{ total }</span>
-    <span data-testid="header-currency-field">Moeda: BRL</span>
-  </header>
-);
+const Header = ({ email, total = 0 }) => {
+  const [totalValue, setTotalValue] = useState(0);
+
+  useEffect(() => {
+    setTotalValue(total);
+  }, [total]);
+
+  return (
+    <header>
+      <span data-testid="email-field">{`E-mail: ${email}`}</span>
+      <span data-testid="total-field">
+        {`Total: ${totalValue}`}
+      </span>
+      <span data-testid="header-currency-field">Moeda: BRL</span>
+    </header>
+  );
+};
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
