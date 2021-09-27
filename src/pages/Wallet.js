cd517318bd5d107b5f05ba28f3127e9da503c71f
Erick Santos <erickosantos.dev@gmail.com>
2021-09-26 22:06:35 -0300

    Req. 9

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddExpense from '../components/AddExpense';
 import Header from '../components/Header';
+import Table from '../components/Table';
 
 const Wallet = () => (
   <div>
     <Header />
     <AddExpense />
+    <Table />
   </div>
 );
 
