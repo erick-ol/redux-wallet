d63bddc515e4154f288f140a077d5309cd14c229
Erick Santos <erickosantos.dev@gmail.com>
2021-10-16 22:22:45 -0300

    Login CSS

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,8 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setUserEmail } from '../actions';
 import validate from '../helper/validate';
+import styles from './css/login.module.css';
+import logoColorDown from '../image/logo-color-down.png';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -22,28 +24,33 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={ (e) => handleSubmit(e) }>
-      <input
-        type="email"
-        data-testid="email-input"
-        placeholder="E-mail"
-        value={ email }
-        onChange={ ({ target }) => setEmail(target.value) }
-      />
-      <input
-        type="password"
-        data-testid="password-input"
-        placeholder="Senha"
-        value={ password }
-        onChange={ ({ target }) => setPassword(target.value) }
-      />
-      <button
-        type="submit"
-        disabled={ !enabled }
-      >
-        Entrar
-      </button>
-    </form>
+    <div className={ styles.login }>
+      <section className={ styles.login_card }>
+        <div className={ styles.login_content }>
+          <img src={ logoColorDown } alt="logo-reduxwallet" />
+          <form onSubmit={ (e) => handleSubmit(e) } className={ styles.form }>
+            <input
+              type="email"
+              data-testid="email-input"
+              placeholder="E-mail"
+              value={ email }
+              onChange={ ({ target }) => setEmail(target.value) }
+            />
+            <input
+              type="password"
+              data-testid="password-input"
+              placeholder="Senha"
+              value={ password }
+              onChange={ ({ target }) => setPassword(target.value) }
+            />
+            <span>Esqueceu a senha?</span>
+            <button type="submit" disabled={ !enabled }>
+              Entrar ⇀
+            </button>
+          </form>
+        </div>
+      </section>
+    </div>
   );
 };
 
